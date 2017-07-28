1db18cd71a06dcafa1b349af72d9166ab1020d5e
Jacob Justice <jacobhj1997@gmail.com>
2017-07-28 20:19:51 +0000

    Functional web scrapper

---
+++ movieScrapper.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("./simple_html_dom.php"); 
+    $url = 'http://www.imdb.com/chart/top';
+    $html = new simple_html_dom();
+    $html->load_file($url); 
+    $table = $html->find('.lister-list', 0);
+    $movies = [];
+    if(!empty($table)){
+    foreach($table->find('tr') as $tableRow) {
+        $titleCol = $tableRow->find('.titleColumn', 0) -> plaintext;
+        $cutIndex = strpos($titleCol, '.');
+        $ranking = substr($titleCol, 0, $cutIndex + 1);
+        $titleCol = substr($titleCol, $cutIndex + 1);
+        $cutIndex = strrpos($titleCol, '(');
+        $title = substr($titleCol, 0, $cutIndex);
+        $titleCol = substr($titleCol, $cutIndex);
+        $year = substr($titleCol, 1, 4);
+        $rating = $tableRow->find('.ratingColumn', 0)->find('strong', 0)->innertext;
+        //echo $ranking + "    " + $title + " - " + $year + " " +$rating +"/10";
+        $row['title'] = $title;
+        $row['rating'] = $rating;
+        $row['ranking'] = $ranking;
+        $row['year'] = $year;
+        array_push($movies, $row);
+    }
+    }
+    echo json_encode($movies);
+ ?>
