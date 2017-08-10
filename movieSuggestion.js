782acdb0cf814956fdba596dcf0033f970d0f0f4
jacobhjustice <jacobhj1997@gmail.com>
2017-08-10 00:27:07 -0500

    Update movieSuggestion.js
    Updates for the UI popup

--- movieSuggestion.js
+++ movieSuggestion.js
@@ -26,6 +26,7 @@ var Movies = {
         var index = Math.floor(Math.random() * Movies.movieList.length);
         var movie = Movies.movieList[index];
         Movies.movieList.splice(index, 1);
+        $("#viewDetails").css('visibility', 'hidden');
         $.ajax({
             type:"get",
             url:"movieDetailsScrapper.php",
@@ -34,16 +35,38 @@ var Movies = {
             },
             success: function(data) {
                 movie.description = data;
+                $("#viewDetails").css('visibility', 'visible');
             }
         });
         return movie;
     },
     loadTemplate: function(movie){
         $("#year").html(movie.year.trim());
-        $("#rating").html(movie.rating.trim() + "/10");
         $("#number").html("#" + movie.ranking.trim());
         $("#name").html(movie.title.trim());
-    }
+        $("#viewDetails").on("click", function(){
+            Movies.popup($("#details"), "closeDetails");
+            $("#detailText").html(movie.description);
+            $("#rating").html(movie.rating.trim() + "/10");
+        });
+    },
+    popup: function($overlay, close){
+        $("#JSwagBackdropDimmer").remove();
+        var $div = $('<div>').attr('id', 'JSwagBackdropDimmer');
+        $div.css('width', '100vw').css('height', '100vh').css('position', 'fixed').css('background-color', 'rgba(25, 25, 25, .5)').css('z-index', '100');
+        
+        var $popup = $("<div>").css('position', 'fixed');
+        $popup.html($overlay.html()).attr('id', 'JSwagPopoverHTML');
+        $div.append($popup);
+        $("html").prepend($div);
+        var h = $popup.css('height');
+        var w = $popup.css('width');
+        $("#JSwagPopoverHTML").css('left', '50%').css('left', '-=' + parseInt(w)/2 + 'px').css('top', '50%').css('top', '-=' + parseInt(h)/2 + 'px');
+        $("#" + close).css('cursor', 'pointer').on('click', function(){
+            $("#JSwagBackdropDimmer").remove();
+        });
+         
+     }
 };
 
 $(document).ready(function(){
